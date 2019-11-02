16722223603169b17b45c66d7d01e9b24f0426ca
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-02 05:03:05 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,9 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist[artistName] = 'songTitle'
 }
 
-
+updatePlaylist(playlist, 'Alison')
+playlist['Slowdive'] = 'Alison'
+playlist['My Bloody Valentine'] = 'Sometimes'
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
